694059f9d4e7999aaf31fa62f766e104423276bf
bramo <bmoosterlee@gmail.com>
2018-09-06 15:02:47 +0200

    Rewrite to find n instead of p sub n

--- Lab1/BramOosterlee/Lab1Ex6.hs
+++ Lab1/BramOosterlee/Lab1Ex6.hs
@@ -3,17 +3,17 @@ import Data.List
 import Test.QuickCheck
 
 -- time started 00:40
-prime :: Integer -> Bool
+prime :: Int -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
   where xs = takeWhile (\ y -> y^2 <= n) primes
 
-primes :: [Integer]
+primes :: [Int]
 primes = 2 : filter prime [3..]
 
-func :: Integer -> Integer
-func x = product(takeWhile (\ y -> y <= x) primes)+1
+func :: Int -> Int
+func n = product(take n primes)+1
 
-counterexamples :: [Integer]
-counterexamples = filter (\ x -> (not (prime (func x)))) primes
+counterexamples :: [Int]
+counterexamples = filter (\ n -> (not (prime (func n)))) [1..]
 
 --The smallest counterexample is the list of primes until 13. 2+3+5+7+11+13+1 = 32
