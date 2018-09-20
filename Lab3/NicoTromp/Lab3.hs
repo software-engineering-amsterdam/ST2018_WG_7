e7454e76b0ed7cc67339a09517cc85979eceae1b
Nico Tromp <nico.tromp@gmail.com>
2018-09-20 06:52:45 +0200

    Add missing implementation for 'contradiction' and define a test for it.

--- Lab3/NicoTromp/Lab3.hs
+++ Lab3/NicoTromp/Lab3.hs
@@ -8,7 +8,7 @@ import Lecture3
 -- ASSIGNMENT 1 - PROPOSITIONAL LOGIC --
 
 contradiction :: Form -> Bool
-contradiction f = False
+contradiction f = not (tautology f)
 
 tautology :: Form -> Bool
 tautology f = all (\ v -> evl v f) (allVals f)
@@ -24,6 +24,9 @@ equiv f g = all (\ v -> evl v f == evl v g) (allVals f)
 allwaysTrue :: Form
 allwaysTrue = head (parse "+(1 -1)")
 
+allwaysFalse :: Form
+allwaysFalse = head (parse "*(1 -1)")
+
 andFunction :: Form
 andFunction = head (parse "*(1 2)")
 
@@ -32,6 +35,7 @@ andAsOrFunction = head (parse "-+(-1 -2)")
 
 -- Tests --
 
+-- contradiction allwaysfalse
 -- tautology allwaysTrue
 -- equiv andFunction andAsOrFunction
 -- entails andFunction allwaysTrue 
