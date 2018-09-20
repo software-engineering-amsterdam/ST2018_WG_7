b0101ef6880121b7dc4e736ae05aa9b042d9e4af
Sjoerd van der Heijden <s.vdh@live.nl>
2018-09-20 10:39:05 +0200

    Finished up exercise 1, with test cases

--- Lab3/SjoerdVanDerHeijden/Sjoerd_Lab3.hs
+++ Lab3/SjoerdVanDerHeijden/Sjoerd_Lab3.hs
@@ -5,13 +5,25 @@ import System.Random
 import Test.QuickCheck
 import Lecture3
 
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
 -----------------------------------------------------------------------------------------
 -- ==EXERCISE 1: LECTURE DEFINITIONS== --
 
-myfunc f = map (\ v -> evl v f) (allVals f)
-
 -- satisfiable :: Form -> Bool
 -- satisfiable f = any (\ v -> evl v f) (allVals f)
+
+{- Helper function: generates the complete allVals for all properties present
+ in both f and g. E.g.: f contains p and q, g contains p and r, combineAllVals
+ creates a valuation for p, q and r.
+-}
+combineAllVals :: Form -> Form -> [Valuation]
+combineAllVals f g = genVals (nub (propNames f ++ propNames g))
+-- More flexible implementation of the previous
+combineAllVals2 :: [Form] -> [Valuation]
+combineAllVals2 forms = genVals (nub (concatMap propNames forms))
 
 contradiction :: Form -> Bool
 contradiction f = not (satisfiable f)
@@ -19,14 +31,21 @@ contradiction f = not (satisfiable f)
 tautology :: Form -> Bool
 tautology f = all (\ v -> evl v f) (allVals f)
 
--- -- | logical entailment 
--- entails :: Form -> Form -> Bool
--- entails f g = all (\ v ->)
+entails :: Form -> Form -> Bool
+entails f g = and [not (evl v f) || evl v g | v <- combineAllVals f g ]
 
--- -- | logical equivalence
--- equiv :: Form -> Form -> Bool
+equiv :: Form -> Form -> Bool
+equiv f g = and [evl v f == evl v g | v <- combineAllVals f g ]
 
+main = do
+    print (satisfiable( head (parse "+(1 2)")))
+    print (contradiction( head (parse "*(1 -1)")))
+    print (tautology( head (parse "+(1 -1)")))
+    print (entails (head (parse "1")) (head (parse "1")))
+    print (equiv (head (parse "1")) (head (parse "1")))
 
-
-
+-- Time: 1h
 -----------------------------------------------------------------------------------------
+-- ==EXERCISE 3: REWRITE TO CNF== --
+
+
