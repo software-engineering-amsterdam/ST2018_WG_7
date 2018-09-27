a8497fcff271e084e64014fb7c062f7d5caa5f24
Nico Tromp <nico.tromp@gmail.com>
2018-09-27 10:13:40 +0200

    Add generator for Set Int build from scratch.

--- Lab4/NicoTromp/Lab4.hs
+++ Lab4/NicoTromp/Lab4.hs
@@ -6,3 +6,27 @@ import Test.QuickCheck
 
 import SetOrd
 
+-- For testing purposes we limit the range of natural numbers
+maxNatural :: Int
+maxNatural = 10
+
+getRandomNatural :: IO Int
+getRandomNatural = getStdRandom (randomR (1, maxNatural))
+
+getRandomIntegers :: Int -> IO [Int]
+getRandomIntegers 0 = return []
+getRandomIntegers n = do
+             x <- getStdRandom (randomR (-maxNatural, maxNatural))
+             xs <- getRandomIntegers (n - 1) 
+             return (x:xs)
+
+convertToSet :: [Int] -> Set Int
+convertToSet [] = emptySet
+convertToSet (x:xs) = insertSet x (convertToSet xs)
+
+integerSetGenerator :: IO (Set Int)
+integerSetGenerator = do
+    n <- getRandomNatural
+    xs <- getRandomIntegers n
+    return (convertToSet (nub xs))
+
