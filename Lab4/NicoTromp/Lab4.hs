110e33e330d724dc93d5a69a377ff76141afea12
Nico Tromp <nico.tromp@gmail.com>
2018-09-27 11:50:10 +0200

    Add quickcheck generator and testing harness.

--- Lab4/NicoTromp/Lab4.hs
+++ Lab4/NicoTromp/Lab4.hs
@@ -6,9 +6,12 @@ import Test.QuickCheck
 
 import SetOrd
 
+
+-- ASSIGNMENT 2
+
 -- For testing purposes we limit the range of natural numbers
 maxNatural :: Int
-maxNatural = 10
+maxNatural = 30
 
 getRandomNatural :: IO Int
 getRandomNatural = getStdRandom (randomR (1, maxNatural))
@@ -20,14 +23,35 @@ getRandomIntegers n = do
              xs <- getRandomIntegers (n - 1) 
              return (x:xs)
 
-convertToSet :: [Int] -> Set Int
-convertToSet [] = emptySet
-convertToSet (x:xs) = insertSet x (convertToSet xs)
-
 integerSetGeneratorFromScratch :: IO (Set Int)
 integerSetGeneratorFromScratch = do
     n <- getRandomNatural
     xs <- getRandomIntegers n
-    return (convertToSet (nub xs))
+    return (Set (nub xs))
 
 -- Time spent: 1:15
+
+instance (Arbitrary a, Eq a) => Arbitrary (Set a) where 
+    arbitrary = do
+        xs <- arbitrary
+        return (Set (nub xs))
+
+-- Time spent: 1:00, trying all different kind of things and failing, until I talked to my teammates :-) 
+
+-- Sorting is needed in order for the grouping to work. Once grouped it is a matter
+-- of ensuring that the groups have a length of 1.
+prop_UniqueElements :: Set Int -> Bool
+prop_UniqueElements (Set xs) = all (\y -> length y == 1) (group (sort xs))
+
+testAssignment2 = do
+    putStrLn "--== Set Int Generator ==--"
+    putStr "Generator from scratch\nElements are unique: "
+    xs <- integerSetGeneratorFromScratch
+    putStrLn (show (prop_UniqueElements xs))
+    putStrLn "\nQuickCheck Generator\nElements are unique: "
+    quickCheck prop_UniqueElements
+
+-- Time spent: 0:30
+
+main = do
+    testAssignment2
