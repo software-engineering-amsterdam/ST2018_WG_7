1c5e5bb5f0cd4642bef67a34f4e723a92aab6993
Nico Tromp <nico.tromp@gmail.com>
2018-10-07 23:53:49 +0200

    Add minimal problem detection.

--- Lab5/NicoTromp/Lab5MinimalizeTesting.hs
+++ Lab5/NicoTromp/Lab5MinimalizeTesting.hs
@@ -8,6 +8,14 @@ import System.Random
 
 {-
 Build upon the code from exercise 2.
+
+If the code from the lecture produces minimal problems this would mean 
+that there is no unique solution when any hint is being removed.
+What we do is that we check for every single hint if there are multiple
+solutions once the hint is erased. If so that would mean the Sudoku is
+a minimal Sudoku.
+
+Time spent: 0:45
 -}
 
 type Row    = Int 
@@ -336,8 +344,22 @@ genProblem n = do ys <- randomize xs
                   return (minimalize n ys)
    where xs = filledPositions (fst n)
 
+-- Here the testing for minimal solutions starts.
+
+hintPositions :: Grid -> [Position]
+hintPositions xs = [ (r,c) | r <- values, c <- filter (/=0) (xs !! (r-1))]
+
+none :: [Bool] -> Bool
+none xs = and (map not xs)
+
+isMinimal :: Sudoku -> Bool
+isMinimal s = none (map uniqueSol [eraseN n p | p <- hintPositions xs])
+  where xs = sud2grid s
+        n = head (initNode xs)
+
 main :: IO ()
 main = do [r] <- rsolveNs [emptyN]
           showNode r
           s  <- genProblem r
           showNode s
+          putStrLn ("Minimal: " ++ (show (isMinimal (fst s))))
