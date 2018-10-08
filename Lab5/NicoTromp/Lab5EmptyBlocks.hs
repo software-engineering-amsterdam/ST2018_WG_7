a22ba3863f6b4ab1b0926ce34abe6e72d8a4b99b
Nico Tromp <nico.tromp@gmail.com>
2018-10-08 11:20:22 +0200

    Empty block solution checks for unique solution.

--- Lab5/NicoTromp/Lab5EmptyBlocks.hs
+++ Lab5/NicoTromp/Lab5EmptyBlocks.hs
@@ -6,20 +6,25 @@ import Lecture5
 
 {-
 Select randomly the blocks to empty and then generate a Sudoku problem with the cleaned Sudoku.
+If there is no unique solution possible this is reported.
 
-Time spent: 0:25
+Time spent: 1:00
 -}
 
 cleanCells :: Node -> [(Row, Column)] -> Node
 cleanCells n []     = n
 cleanCells n (x:xs) = eraseN (cleanCells n xs) x
 
-cleanBlocks :: Node -> [Int] -> Node
-cleanBlocks n xs = cleanCells n (concatMap (\x -> [(r,c) | r <- blocks !! (x `div` 3), c <- blocks !! (x `rem` 3)]) xs)
+cleanBlocks :: Node -> [(Row, Column)] -> Node
+cleanBlocks n xs = cleanCells n (concatMap (\(r,c) -> [(r',c') | r' <- blocks !! r, c' <- blocks !! c]) xs)
 
-genEmptyBlockSudoku :: Int -> IO ()
-genEmptyBlockSudoku n = do [r] <- rsolveNs [emptyN]
-                           showNode r
-                           bs <- randomize [0..8]
-                           s  <- genProblem (cleanBlocks r (take n bs))
-                           showNode s
+checkEmptyBlockSudoku :: Int -> IO ()
+checkEmptyBlockSudoku n = do [r] <- rsolveNs [emptyN]
+                             showNode r
+                             xs <- randomize [(r,c) | r <- [0..2], c <- [0..2]]
+                             let xs' = take n xs
+                             let r' = cleanBlocks r xs'
+                             s  <- genProblem r'
+                             if uniqueSol r' then showNode s
+                             else putStrLn ("--- Failed.\n There is no unique solution possible whith clean blocks: "
+                                           ++ (show (map (\(r,c) -> (r+1,c+1)) xs'))) 
