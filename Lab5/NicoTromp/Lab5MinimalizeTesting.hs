17a3af59a078cc0011bb5ca4253406fcd36cec05
Nico Tromp <nico.tromp@gmail.com>
2018-10-08 07:44:20 +0200

    Switch to existing functions.

--- Lab5/NicoTromp/Lab5MinimalizeTesting.hs
+++ Lab5/NicoTromp/Lab5MinimalizeTesting.hs
@@ -17,19 +17,15 @@ Given the code from the lecture we can have the existing code generate
 a Sudoku problem and then using the technique described above to test
 if it is a minimal Soduku.
 
-Time spent: 0:30
+Time spent: 0:40, optimizing the solution by reusing existing methods.
 -}
-
-hintPositions :: Grid -> [(Row, Column)]
-hintPositions xs = [ (r,c) | r <- values, c <- filter (/=0) (xs !! (r-1))]
 
 none :: [Bool] -> Bool
 none = all not
 
 isMinimal :: Sudoku -> Bool
-isMinimal s = none (map uniqueSol [eraseN n p | p <- hintPositions xs])
-  where xs = sud2grid s
-        n = head (initNode xs)
+isMinimal s = none (map uniqueSol [eraseN n p | p <- filledPositions s])
+  where n = head (initNode (sud2grid s))
 
 minimalizeTesting :: IO ()
 minimalizeTesting = do [r] <- rsolveNs [emptyN]
