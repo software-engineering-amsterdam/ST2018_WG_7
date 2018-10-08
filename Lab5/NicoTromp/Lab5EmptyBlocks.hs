111ec71908c7644bc2285f788806b0191286a7b9
Nico Tromp <nico.tromp@gmail.com>
2018-10-08 10:30:22 +0200

    Combine block selection and position determination into single function.

--- Lab5/NicoTromp/Lab5EmptyBlocks.hs
+++ Lab5/NicoTromp/Lab5EmptyBlocks.hs
@@ -7,22 +7,19 @@ import Lecture5
 {-
 Select randomly the blocks to empty and then generate a Sudoku problem with the cleaned Sudoku.
 
-Time spent: 0:20
+Time spent: 0:25
 -}
 
 cleanCells :: Node -> [(Row, Column)] -> Node
 cleanCells n []     = n
 cleanCells n (x:xs) = eraseN (cleanCells n xs) x
 
-cleanBlocks :: Node -> [(Row, Column)] -> Node
-cleanBlocks n xs = cleanCells n [ (r',c') | (r,c) <- xs, r' <- blocks !! r, c' <- blocks !! c]
-
-selectEmptyBlocks :: Int -> (Row, Column)
-selectEmptyBlocks n = (n `div` 3, n `rem` 3)
+cleanBlocks :: Node -> [Int] -> Node
+cleanBlocks n xs = cleanCells n (concatMap (\x -> [(r,c) | r <- blocks !! (x `div` 3), c <- blocks !! (x `rem` 3)]) xs)
 
 genEmptyBlockSudoku :: Int -> IO ()
 genEmptyBlockSudoku n = do [r] <- rsolveNs [emptyN]
                            showNode r
                            bs <- randomize [0..8]
-                           s  <- genProblem (cleanBlocks r [ selectEmptyBlocks b | b <- take n bs])
+                           s  <- genProblem (cleanBlocks r (take n bs))
                            showNode s
