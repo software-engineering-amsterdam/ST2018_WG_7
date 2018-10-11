99df5ad43a03494144ace9e873972327d65b2119
Bram Oosterlee <bmoosterlee@gmail.com>
2018-10-11 10:01:43 +0200

    Refactor assignment 1, 3

--- Lab6/BramOosterlee/Lab6.hs
+++ Lab6/BramOosterlee/Lab6.hs
@@ -6,18 +6,14 @@ import Lecture6
 
 --Assignment 1, time 00:05
 exM :: Integer -> Integer -> Integer -> Integer
-exM x y n	| y == 0	= 1 `mod` n
-			| otherwise = (exM x (y-1) n) * (x `mod` n)
+exM x y n | y == 0    = 1 `mod` n
+          | otherwise = (Lab6.exM x (y-1) n) * (x `mod` n)
 
 --Assignment 2, time
+-- todo: optimize to find the composites more efficiently
 
---Assignment 3, time 00:05
-mrComposite :: Integer -> Integer -> Bool mrComposite x n = let
-(r,s) = decomp (n-1)
-fs = takeWhile (/= 1)
-(map (\ j -> exM x (2^j*s) n) [0..r]) in
-exM x s n /= 1 && last fs /= (n-1) -- if (xs mod n) == 1 → (xs mod n)pow(2,r) == 1
--- if last fs == (n-1) → xpow(2,r)*s does not fail Fermat
-
+--Assignment 3, time 00:20
 composites :: [Integer]
-composites = [(a, b) | a <- [1..], b <- [1..], (mrComposite a b)]
+composites = [n | n <- [3..], any (==True) [mrComposite x n | x <- [2..n-1]]]
+
+--Assignment 4, time 9:40
